4de357c9dd50cf6e3528e02f803f392e257154f0
Isha Mehrotra <isha.mehrotra@hotmail.com>
2017-09-01 23:13:46 -0400

    added to readme

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # Easy Click Tweet
 An application that allows the user to share links to pre-written tweets that can be posted
 
+imehrotra.github.io/easy-click-tweet/
+
 ## Purpose
+Over the last year, Twitter has become an important communication platform for activist groups.  The tactic of "Twitter Storms" and "Thunderclaps" has led to a trend of several people posting the same thing simultaneously to better flood the Internet with their message.  Currently many groups use websites like Click To Tweet which offers limited functionality and charges money for analytics access.
+
+To better serve activists, I created a web app that outputs a shortened url to the scripted Twitter message that has an attached emoji keyboard.  To use it, just type in your message, hit get link, and copy the outputted url!
 
 ## Process
+TO DO
 
 ## Technologies
+TODO
+
+* Twitter Web Intents
+* Google URL Shortener API
+* EmojiPicker
+
+CSS Grid
 
 ## Future Development
+TODO
+
+* Parse for # and url
+* Better design
+* Allow for click or copy link
+* Add link analytics (api)
+* Retweet functionality
+* Media functionality
--- index.js
+++ index.js
@@ -1,56 +1,3 @@
-/*$(document).ready(function() {
-      // $('#create').click(function(e) {
-      //   e.preventDefault();
-      //   $('#text-custom-trigger').emojiPicker({
-      //     width: '300px',
-      //     height: '200px',
-      //     button: false
-      //   });
-      // });
-	    var text_max = 140;
-	    $('#textarea_feedback').html(text_max + ' characters remaining');
-	    $('#textarea_feedback').attr("style", "font-size:16px; color:white;");
-	    $('#textarea').keyup(function() {
-	        var text_length = $('#textarea').val().length;
-	        var text_remaining = text_max - text_length;
-	        $('#textarea_feedback').html(text_remaining + ' characters remaining');
-	    // console.log(document.getElementById("textarea").value);
-	    });
-	});
-// on click event...or rather submit form...
-$(document).on('click', '#click', function() {
-	var string = document.getElementById("textarea").value;
-	// console.log(string);
-	var res = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(string);
-	console.log(res);
-	urlshort(res);
-});
-//retweet functionality
-//url shortener
-function urlshort(res) {
-$.ajax({
-        url: 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=http://goo.gl/fbsS&key=AIzaSyBFmYcn3yahS8ao6Zabkc7kaRbfEs_sIhE',
-        type: 'POST',
-        contentType: 'application/json; charset=utf-8',
-        data: '{ longUrl: "' + res +'"}',
-        dataType: 'json',
-        success: function(response) {
-            // console.log(response);
-            console.log(response.id);
-        }
-     });
-}
-// TO DO
-// da fuq
-// parse for # and url
-// dynamic rows and cols
-// layout/design
-// retweet functionality
-// emoji keyboard
-// auto shorten urls
-*/
-
-
 
 	$(document).ready(function() {
 
@@ -92,6 +39,7 @@ $.ajax({
             // console.log(response);
             console.log(response.id);
             var out = document.getElementById("out");
+            out.color="white";
             out.innerHTML=response.id;
         }
      });
